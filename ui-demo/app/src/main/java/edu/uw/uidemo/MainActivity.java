fed269ce6898e6cd66affc2ce085c88a25660055
kxchiu <kevinx_chiu@hotmail.com>
2016-01-28 12:20:42 -0800

    update from lecture

--- ui-demo/app/src/main/java/edu/uw/uidemo/MainActivity.java
+++ ui-demo/app/src/main/java/edu/uw/uidemo/MainActivity.java
@@ -1,11 +1,21 @@
 package edu.uw.uidemo;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_PICTURE_CODE = 1;
 
+    private boolean hideActionBar = false;
+
     private int mClickCount = 0;
 
     @Override
@@ -21,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //ActionBar actionBar = getSupportActionBar(); //for reference
+
 
         //View launchButton = findViewById(R.id.btnLaunch);
         //View callButton = findViewById(R.id.btnDial);
@@ -44,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         Log.v(TAG, "Call button pressed");
 
         //implicit intent
-        Intent intent = new Intent(Intent.ACTION_SEND);
+        Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:206-685-1622"));
 
         if (intent.resolveActivity(getPackageManager()) != null) {
@@ -67,8 +79,75 @@ public class MainActivity extends AppCompatActivity {
         //...
     }
 
+
+    private static final int CLICK_NOTIFICATION_ID = 0;
+
     public void clickerPressed(View v) {
         Log.v(TAG, "Clicker button pressed");
         //...
+
+        //SHOW Notification
+        //Required: icon, content title, content text
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_stat_name2)
+                .setContentTitle("Click Counting")
+                .setContentText("I work!")
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        //TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the Intent that starts the Activity to the top of the stack
+        //stackBuilder.addNextIntent(resultIntent);
+        //PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(CLICK_NOTIFICATION_ID, mBuilder.build());
+
+        //get access to actionbar
+        /*
+        ActionBar actionBar = getSupportActionBar(); //for reference
+
+        if (hideActionBar == false) {
+            hideActionBar = true;
+            actionBar.hide();
+        } else {
+            hideActionBar = false;
+            actionBar.show();
+        }
+        */
+
+    }
+
+    //create option menu and set the layout
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //instatiate a menu inflater and inflate
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate((R.menu.main_menu), menu);
+
+        return true;
+    }
+
+    //decides what to do when the item is clicked based on its id
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch ((item.getItemId())) {
+            case R.id.menu_item1:
+                callNumber(null);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
